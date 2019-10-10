3daae6d71cc171b324c061d7321fa60bc58654c7
jbrandt <jbrandt@nd.edu>
2019-10-10 16:31:14 -0400

    modified:   step1.sh

--- step1.sh
+++ step1.sh
@@ -7,3 +7,5 @@ echo "lowest earner:"
 cat wages.csv | cut -d , -f 1,2,4 | sed 's/,/ /g' | sort -k 3 -nr | grep -v "gender" | tail -n 1
 echo "number of women top ten:"
 cat wages.csv | cut -d , -f 1,2,4 | sed 's/,/ /g' | sort -k 3 -nr | head -n 10 | grep -e female | wc -l
+
+cat wages.csv |cut -d , -f 3,4 | sed 's/,/ /g' | grep -v "wage" | sort -k 1 -nr 
