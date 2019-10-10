a5538be9077f721c00805ce00ceee126dea51754
Michael ODea <modea4@nd.edu>
2019-10-10 16:07:03 -0400

    step 1 and two merged

--- step1.sh
+++ step1.sh
@@ -1,6 +1,9 @@
 # usage: bash step1.sh
 
 cat wages.csv | cut -d , -f 1,2 | sed 's/,/ /g' | grep -v "gender" | sort -u | sort -V > gender_yearsExperience.txt
-
-
-
+echo "highest earner:"
+cat wages.csv | cut -d , -f 1,2,4 | sed 's/,/ /g' | sort -k 3 -nr | head -n 1
+echo "lowest earner:"
+cat wages.csv | cut -d , -f 1,2,4 | sed 's/,/ /g' | sort -k 3 -nr | grep -v "gender" | tail -n 1
+echo "number of women top ten:"
+cat wages.csv | cut -d , -f 1,2,4 | sed 's/,/ /g' | sort -k 3 -nr | head -n 10 | grep -e female | wc -l
