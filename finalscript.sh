72744688fb9a93321d7f02dfa90fde869a1491e6
Michael ODea <modea4@nd.edu>
2019-10-10 17:00:01 -0400

    finished

--- finalscript.sh
+++ finalscript.sh
@@ -9,8 +9,10 @@ echo "number of women top ten:"
 cat wages.csv | cut -d , -f 1,2,4 | sed 's/,/ /g' | sort -k 3 -nr | head -n 10 | grep -e female | wc -l
 
 
-var1=$(cat wages.csv | cut -d , -f 3,4 | sed 's/,/ /g' | grep -v "wage" | sort -k 1 -nr | grep -e "^16" | sort -k 2 -nr | tail -n 1) 
-var2=$(cat wages.csv | cut -d , -f 3,4 | sed 's/,/ /g' | grep -v "wage" | sort -k 1 -nr | grep -e "^12" | sort -k 2 -nr | tail -n 1)
+var1=$(cat wages.csv | cut -d , -f 3,4 | sed 's/,/ /g' | grep -v "wage" | sort -k 1 -nr | grep -e "^16" | sort -k 2 -nr | tail -n 1 | cut -d " " -f 2) 
+var2=$(cat wages.csv | cut -d , -f 3,4 | sed 's/,/ /g' | grep -v "wage" | sort -k 1 -nr | grep -e "^12" | sort -k 2 -nr | tail -n 1 | cut -d " " -f 2)
 echo "minimum wage difference"
 echo "$var1 - $var2" | bc
+
+
 
