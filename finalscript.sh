cc4340eda93d94b7a693315e7d1b24fee271773c
Michael ODea <modea4@nd.edu>
2019-10-10 16:47:39 -0400

    final script with error

---
+++ finalscript.sh
@@ -0,0 +1,16 @@
+# usage: bash step1.sh
+
+cat wages.csv | cut -d , -f 1,2 | sed 's/,/ /g' | grep -v "gender" | sort -u | sort -V > gender_yearsExperience.txt
+echo "highest earner:"
+cat wages.csv | cut -d , -f 1,2,4 | sed 's/,/ /g' | sort -k 3 -nr | head -n 1
+echo "lowest earner:"
+cat wages.csv | cut -d , -f 1,2,4 | sed 's/,/ /g' | sort -k 3 -nr | grep -v "gender" | tail -n 1
+echo "number of women top ten:"
+cat wages.csv | cut -d , -f 1,2,4 | sed 's/,/ /g' | sort -k 3 -nr | head -n 10 | grep -e female | wc -l
+
+
+var1=$(cat wages.csv | cut -d , -f 3,4 | sed 's/,/ /g' | grep -v "wage" | sort -k 1 -nr | grep -e "^16" | sort -k 2 -nr | tail -n 1) 
+var2=$(cat wages.csv | cut -d , -f 3,4 | sed 's/,/ /g' | grep -v "wage" | sort -k 1 -nr | grep -e "^12" | sort -k 2 -nr | tail -n 1)
+echo "minimum wage difference"
+echo "$var1 - $var2" | bc
+
