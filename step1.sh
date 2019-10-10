267a8cee8876b18b917fd06c3db4415d76dd5215
Michael ODea <modea4@nd.edu>
2019-10-10 16:48:56 -0400

    mine

--- step1.sh
+++ step1.sh
@@ -7,3 +7,10 @@ echo "lowest earner:"
 cat wages.csv | cut -d , -f 1,2,4 | sed 's/,/ /g' | sort -k 3 -nr | grep -v "gender" | tail -n 1
 echo "number of women top ten:"
 cat wages.csv | cut -d , -f 1,2,4 | sed 's/,/ /g' | sort -k 3 -nr | head -n 10 | grep -e female | wc -l
+
+
+var1=$(cat wages.csv | cut -d , -f 3,4 | sed 's/,/ /g' | grep -v "wage" | sort -k 1 -nr | grep -e "^16" | sort -k 2 -nr | tail -n 1) 
+var2=$(cat wages.csv | cut -d , -f 3,4 | sed 's/,/ /g' | grep -v "wage" | sort -k 1 -nr | grep -e "^12" | sort -k 2 -nr | tail -n 1)
+echo "minimum wage difference"
+echo "$var1 - $var2" | bc
+
